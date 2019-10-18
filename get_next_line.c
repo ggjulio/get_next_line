f74b2b2a780d2c016124d54d071791248a6e5660
Julio Gonzalez <gonzalez.julio8959@gmail.com>
2019-10-19 00:50:43 +0200

    Go to slepp

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,42 @@
 /*   By: juligonz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 16:51:24 by juligonz          #+#    #+#             */
-/*   Updated: 2019/10/10 16:51:38 by juligonz         ###   ########.fr       */
+/*   Updated: 2019/10/19 00:50:20 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line_h"
 
+static int		error(int fd, char **s)
+{
+	if (fd < 0)
+		return (-1)
+}
+
+static char		*read_line()
+{
+	char	buffer[BUFFER_SIZE + 1];
+	int		ret;
+
+	while ((ret = read(fd, buffer, BUFFER_SIZE)))
+	{
+		buffer[ret] = '\0';
+		// mon strdupcat
+	}
+}
 
 int		get_next_line(int fd, char **line)
 {
+	static char	*s;
+	size_t		i;
 
+	if (error == -1)
+		return (-1);
+//	if (*s)
+		// already call func
+		//do job
+
+	str = readline();
+		
+	
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,16 @@
 /*   By: juligonz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 16:51:14 by juligonz          #+#    #+#             */
-/*   Updated: 2019/10/10 16:51:16 by juligonz         ###   ########.fr       */
+/*   Updated: 2019/10/14 01:29:23 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		get_next_line(int fd, char **line);
+
+#endif
