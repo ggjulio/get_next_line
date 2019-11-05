052f0cb40970e9ba5c31a7ad1a1c8a2f0a7ab941
Julio GONZALEZ GOMEZ <juligonz@e3r10p18.42.fr>
2019-11-05 19:55:26 +0100

    goto home

---
+++ gnlkiller/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juligonz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/10 16:51:14 by juligonz          #+#    #+#             */
+/*   Updated: 2019/11/04 15:18:19 by juligonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct	s_str
+{
+	char	s[BUFFER_SIZE];
+	int		len;
+	int		old_len;
+}				t_str;
+
+int				get_next_line(int fd, char **line);
+
+#endif
